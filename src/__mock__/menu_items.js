aab3ba386b6d4ea54890559a813a7aa91b0f59a4
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-21 19:02:01 +0100

    mock the menu data

---
+++ src/__mock__/menu_items.js
@@ -0,0 +1,28 @@
+const menuItems = [
+    {
+        id: 'mi0001',
+        title: 'All',
+        icon: 'tasks',
+        color: 'gray',
+        link: '/',
+        type: 'link'
+    },
+    {
+        id: 'mi0002',
+        title: 'Todo',
+        icon: 'xmark',
+        color: 'red',
+        link: '/remain-todo',
+        type: 'link'
+    },
+    {
+        id: 'mi0003',
+        title: 'Completed',
+        icon: 'check',
+        color: 'green',
+        link: '/completed-todo',
+        type: 'link'
+    }
+]
+
+export default menuItems
